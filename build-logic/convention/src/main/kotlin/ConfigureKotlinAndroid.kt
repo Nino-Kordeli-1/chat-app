9ae2f081703165241e4efc52da41dfd65ad8954e
tdspace01 <tdspacein@gmail.com>
2026-08-10 17:41:20 +0400

    Build-Logic Implementation

---
+++ build-logic/convention/src/main/kotlin/ConfigureKotlinAndroid.kt
@@ -0,0 +1,80 @@
+import ConventionConstants.Bundles
+import ConventionConstants.Libraries
+import com.android.build.api.dsl.ApplicationExtension
+import com.android.build.api.dsl.LibraryExtension
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
+
+internal val Project.libs: VersionCatalog
+    get() = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+internal fun Project.configureAndroidApplication(extension: ApplicationExtension) {
+    pluginManager.apply(ConventionConstants.Plugins.KOTLIN_SERIALIZATION)
+
+    extension.apply {
+        compileSdk = ConventionConstants.COMPILE_SDK
+        defaultConfig {
+            minSdk = ConventionConstants.MIN_SDK
+            targetSdk = ConventionConstants.TARGET_SDK
+        }
+        compileOptions {
+            sourceCompatibility = ConventionConstants.JAVA_VERSION
+            targetCompatibility = ConventionConstants.JAVA_VERSION
+        }
+    }
+    configureSharedKotlinAndDependencies()
+}
+
+internal fun Project.configureAndroidLibrary(extension: LibraryExtension) {
+    pluginManager.apply(ConventionConstants.Plugins.KOTLIN_SERIALIZATION)
+
+    extension.apply {
+        compileSdk = ConventionConstants.COMPILE_SDK
+        defaultConfig {
+            minSdk = ConventionConstants.MIN_SDK
+        }
+        compileOptions {
+            sourceCompatibility = ConventionConstants.JAVA_VERSION
+            targetCompatibility = ConventionConstants.JAVA_VERSION
+        }
+    }
+    configureSharedKotlinAndDependencies()
+}
+
+private fun Project.configureSharedKotlinAndDependencies() {
+    extensions.configure<KotlinAndroidProjectExtension> {
+        compilerOptions {
+            jvmTarget.set(ConventionConstants.JVM_TARGET)
+        }
+    }
+
+    dependencies {
+        implementation(libs, Libraries.KOIN_ANDROID)
+        implementationBundle(libs, Bundles.NAVIGATION)
+        testImplementationBundle(libs, Bundles.TESTING)
+    }
+}
+
+internal fun Project.configureComposeDependencies() {
+    dependencies {
+        implementationPlatform(libs, Libraries.COMPOSE_BOM)
+        implementationBundle(libs, Bundles.COMPOSE)
+        implementation(libs, Libraries.KOIN_COMPOSE)
+        debugImplementationBundle(libs, Bundles.COMPOSE_DEBUG)
+    }
+}
+
+internal fun Project.configureStorageDependencies() {
+    pluginManager.apply(ConventionConstants.Plugins.KSP)
+
+    dependencies {
+        implementation(libs, Libraries.ROOM_RUNTIME)
+        implementation(libs, Libraries.ROOM_KTX)
+        ksp(libs, Libraries.ROOM_COMPILER)
+    }
+}
---
+++ build-logic/convention/src/main/kotlin/ConventionConstants.kt
@@ -0,0 +1,43 @@
+import org.gradle.api.JavaVersion
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+object ConventionConstants {
+    const val COMPILE_SDK = 37
+    const val MIN_SDK = 24
+    const val TARGET_SDK = 37
+    val JAVA_VERSION = JavaVersion.VERSION_17
+    val JVM_TARGET = JvmTarget.JVM_17
+
+    object Plugins {
+        const val ANDROID_APPLICATION = "com.android.application"
+        const val ANDROID_LIBRARY = "com.android.library"
+        const val KOTLIN_JVM = "org.jetbrains.kotlin.jvm"
+        const val KOTLIN_SERIALIZATION = "org.jetbrains.kotlin.plugin.serialization"
+        const val KOTLIN_COMPOSE = "org.jetbrains.kotlin.plugin.compose"
+        const val JAVA_LIBRARY = "java-library"
+        const val KSP = "com.google.devtools.ksp"
+    }
+
+    object Configurations {
+        const val IMPLEMENTATION = "implementation"
+        const val TEST_IMPLEMENTATION = "testImplementation"
+        const val DEBUG_IMPLEMENTATION = "debugImplementation"
+        const val KSP = "ksp"
+    }
+
+    object Libraries {
+        const val KOIN_ANDROID = "koin-android"
+        const val KOIN_COMPOSE = "koin-androidx-compose"
+        const val COMPOSE_BOM = "androidx-compose-bom"
+        const val ROOM_RUNTIME = "androidx-room-runtime"
+        const val ROOM_KTX = "androidx-room-ktx"
+        const val ROOM_COMPILER = "androidx-room-compiler"
+    }
+
+    object Bundles {
+        const val NAVIGATION = "navigation"
+        const val TESTING = "testing"
+        const val COMPOSE = "compose"
+        const val COMPOSE_DEBUG = "compose-debug"
+    }
+}

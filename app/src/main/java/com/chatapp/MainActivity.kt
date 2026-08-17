629d6a6e61794621ac0107f3758ed8fcc9452dc5
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-08-17 10:42:54 +0400

    Made changes in main activity

--- app/src/main/java/com/chatapp/MainActivity.kt
+++ app/src/main/java/com/chatapp/MainActivity.kt
@@ -4,15 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.ui.Modifier
-import androidx.navigation3.runtime.entryProvider
 import com.chatapp.designsystem.theme.ChatAppTheme
 import com.chatapp.navigation.ChatAppNavHost
-import com.chatapp.navigation.entries.auth.authEntry
-import com.chatapp.navigation.entries.biometrics.biometricsEntry
-import com.chatapp.navigation.entries.home.homeEntry
-import com.chatapp.navigation.entries.verification.verificationEntry
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +13,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ChatAppTheme {
-                ChatAppNavHost(
-                    modifier = Modifier.fillMaxSize(),
-                    entryProvider = entryProvider {
-                        homeEntry()
-                        authEntry()
-                        verificationEntry()
-                        biometricsEntry()
-                    }
-                )
+                ChatAppNavHost()
             }
         }
     }

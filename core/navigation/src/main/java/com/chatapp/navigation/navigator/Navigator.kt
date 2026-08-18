6600beb08808dcf7648c28a6498ccc40e0a17dd9
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-08-18 23:49:56 +0400

    Removed redundant value

--- core/navigation/src/main/java/com/chatapp/navigation/navigator/Navigator.kt
+++ core/navigation/src/main/java/com/chatapp/navigation/navigator/Navigator.kt
@@ -1,14 +1,9 @@
 package com.chatapp.navigation.navigator
 
-import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.navigation3.runtime.NavKey
 
 interface Navigator {
-    fun navigateTo(route: NavKey)
-    fun clearAndNavigate(route: NavKey)
-    fun navigateBack()
-}
-
-val localNavigator = staticCompositionLocalOf<Navigator> {
-    error("No Navigator provided")
+    fun push(route: NavKey)
+    fun replaceAll(route: NavKey)
+    fun pop()
 }

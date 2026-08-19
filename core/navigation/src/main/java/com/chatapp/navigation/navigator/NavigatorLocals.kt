3ca41bde8014698db54bd97db6b5a27017b432c2
Nino-Kordeli-1 <nino.kordeli@space.ge>
2026-08-19 12:30:40 +0400

    Made changes to NavigatorLocals

--- core/navigation/src/main/java/com/chatapp/navigation/navigator/NavigatorLocals.kt
+++ core/navigation/src/main/java/com/chatapp/navigation/navigator/NavigatorLocals.kt
@@ -1,15 +1,39 @@
 package com.chatapp.navigation.navigator
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.navigation3.runtime.NavKey
 
-val LocalGlobalNavigator = staticCompositionLocalOf<Navigator?> { null }
+object NoOpNavigator : Navigator {
+    override fun push(route: NavKey) {
+        warning("push($route)")
+    }
 
-val LocalFlowNavigator = compositionLocalOf<Navigator?> { null }
+    override fun replaceAll(route: NavKey) {
+        warning("replaceAll($route)")
+    }
+
+    override fun pop() {
+        warning("pop()")
+    }
+
+}
+
+private fun warning(action: String) {
+    Log.e(
+        "NoOpNavigator",
+        "$action called without Navigator provided"
+    )
+}
+
+val LocalGlobalNavigator = staticCompositionLocalOf<Navigator> { NoOpNavigator }
+
+val LocalFlowNavigator = compositionLocalOf<Navigator> { NoOpNavigator }
 
 @Composable
-fun globalNavigator(): Navigator? = LocalGlobalNavigator.current
+fun globalNavigator(): Navigator = LocalGlobalNavigator.current
 
 @Composable
-fun localNavigator(): Navigator? = LocalFlowNavigator.current
+fun localNavigator(): Navigator = LocalFlowNavigator.current
